5dfefd35cc7cb8193bf48277e1bbb7753066f4c6
JonChng03 <jonathanchngyk@gmail.com>
2024-04-22 19:55:11 +0800

    App created. Unstyled.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
+import { marked } from 'marked';
 import './App.css';
 
 function App() {
+
+  const [markdownText, setMarkdownText] = useState(`# Header (H1 size)
+
+  ## Sub header (H2 size)
+  
+  [Link](https://example.com)
+  
+  Inline code: \`console.log('Hello, World!')\`
+  
+  \`\`\`
+  // Code block
+  function greet(name) {
+    return 'Hello, ' + name + '!';
+  }
+  \`\`\`
+  
+  - List item 1
+  - List item 2
+  - List item 3
+  
+  > Blockquote
+  
+  ![Image](https://example.com/image.jpg)
+  
+  **Bolded text**`);;
+
+  const handleChange = (e) => {
+    setMarkdownText(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Editor markdownText={markdownText} handleChange={handleChange} />
+      <Preview markdownText={markdownText} />
+    </div>
+  );
+}
+
+function Editor(props) {
+  return (
+    <textarea id="editor" value={props.markdownText} onChange={props.handleChange} rows="10" cols="50">
+      
+    </textarea>
+  );
+}
+
+function Preview(props){
+  return (
+    <div id="preview" dangerouslySetInnerHTML={{__html: marked(props.markdownText)}}>
     </div>
   );
 }
